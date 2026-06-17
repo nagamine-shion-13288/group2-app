a5d6120ebb058978c80d404220c34825bbe45b7f
miyaokana <k.miyao@tbc-inc.co.jp>
2026-06-17 17:17:04 +0900

    aaaaaa

--- routes/product_route.php
+++ routes/product_route.php
@@ -6,5 +6,8 @@ use Illuminate\Support\Facades\Route;
 // ②【必須】あなたが作ったProductControllerの住所
 use App\Http\Controllers\ProductController;
 
+use App\Http\Controllers\CartController;
+
 // ③【必須】ユーザーがアクセスしたときの交通整理（ルート定義）
 Route::get('/products', [ProductController::class, 'products']);
+
