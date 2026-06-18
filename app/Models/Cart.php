0383843b36d360ee9bcf5e6a8a9f357401f69e5f
Shion Nagamine <715-student014@la-study.com>
2026-06-18 13:43:33 +0900

    カート画面プロダクト接続２

--- app/Models/Cart.php
+++ app/Models/Cart.php
@@ -4,37 +4,28 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-
+use App\Models\Product;
 
 class Cart extends Model
 {
-    /**
-     * 複合主キーのため incrementing を無効化
-     */
     public $incrementing = false;
 
-    /**
-     * 一括代入可能なカラム
-     */
     protected $fillable = [
         'user_id',
         'product_id',
         'quantity',
     ];
 
-    /**
-     * ユーザーとのリレーション
-     */
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
     /**
-     * 商品とのリレーション（仮）
+     * 商品とのリレーション
      */
     public function product(): BelongsTo
     {
-        return $this->belongsTo('App\Models\Product');
+        return $this->belongsTo(Product::class);
     }
 }
