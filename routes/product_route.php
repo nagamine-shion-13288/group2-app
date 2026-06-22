a9c1796d3028c39d6b8c27dc8adc4681a90eb0ee
MasamuneSuzuki <m.suzuki@tbc-inc.co.jp>
2026-06-22 10:31:05 +0900

    検索機能ですよ

--- routes/product_route.php
+++ routes/product_route.php
@@ -7,8 +7,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 
 
-// ③【必須】ユーザーがアクセスしたときの交通整理（ルート定義）
-Route::get('/products', [ProductController::class, 'products']);
+// 商品一覧画面（兼、検索結果表示）
+Route::get('/products', [ProductController::class, 'products'])->name('products.index');
 
 //商品詳細画面用
 Route::get('/products/{id}', [ProductController::class, 'show'])->name('products.show');
